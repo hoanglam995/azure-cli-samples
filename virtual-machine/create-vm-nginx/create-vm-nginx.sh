af58f06b03db5f569d38264bcb94eb56780ea459
Hoàng Lâm <hoanglam995@gmail.com>
2021-11-05 10:46:56 +0700

    Update create-vm-nginx.sh

--- virtual-machine/create-vm-nginx/create-vm-nginx.sh
+++ virtual-machine/create-vm-nginx/create-vm-nginx.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Create a resource group.
-az group create --name myResourceGroup --location westeurope
+az group create --name rg-LamHT13 --location westeurope
 
 # Create a new virtual machine, this creates SSH keys if not present.
-az vm create --resource-group myResourceGroup --name myVM --image UbuntuLTS --generate-ssh-keys
+az vm create --resource-group rg-LamHT13 --name LamHT13VM --image UbuntuLTS --generate-ssh-keys
 
 # Open port 80 to allow web traffic to host.
-az vm open-port --port 80 --resource-group myResourceGroup --name myVM 
+az vm open-port --port 80 --resource-group rg-LamHT13 --name LamHT13VM 
 
 # Use CustomScript extension to install NGINX.
 az vm extension set \
   --publisher Microsoft.Azure.Extensions \
   --version 2.0 \
   --name CustomScript \
-  --vm-name myVM \
-  --resource-group myResourceGroup \
+  --vm-name LamHT13VM \
+  --resource-group rg-LamHT13 \
   --settings '{"commandToExecute":"apt-get -y update && apt-get -y install nginx"}'
